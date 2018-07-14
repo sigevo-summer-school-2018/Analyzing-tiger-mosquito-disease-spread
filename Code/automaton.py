139e6f9f5ff6727fff4413be8d26babee6a96863
Hwiyeon Cho <hwyn.cho@gmail.com>
2018-07-15 00:20:57 +0900

    :pencil: Write codes in Codes/automaton.py

--- Code/automaton.py
+++ Code/automaton.py
@@ -1,24 +1,64 @@
-# initial_lettice is a two dimensional array of states (0,1)
-# rules is a 2d matrix with 2 rows: the first row states the probabilities for getting sick,
-#                                   the second one states the probabilities for becoming healthy.
-# max_t defines the maximum number of steps
-# Returns: the lettice after the last step
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Modules for Cellular Automata
+
+Created by mitssi on 2018.07
+Ported to Python by hwyncho on 2018.07
+Last updated by hwyncho on 2018.07
+"""
+import numpy as np
+import random
+
+__N_NEIGHBOR__ = 1
+random.seed = 1
+
+
+def __find(lettice):
+    ret = 0
+    for i in range(-__N_NEIGHBOR__, __N_NEIGHBOR__):
+        for j in range(-__N_NEIGHBOR__, __N_NEIGHBOR__):
+            if (i != 0) or (j != 0):
+                try:
+                    ret += 1
+                except IndexError:
+                    pass
+    return ret
 
 
 def run(initial_lettice, rules, max_t):
     """
-    Set docstring here.
+    Run cellular automaton
 
     Parameters
     ----------
-    initial_lettice: a two dimensional array of states (0,1)
-    rules: the first row states the probabilities for getting sick,
-           the second one states the probabilities for becoming healthy.
-    max_t: the maximum number of steps
+    initial_lettice: list
+        a two dimensional array of states (0, 1)
+    rules: list
+        the first row states the probabilities for getting sick,
+        the second one states the probabilities for becoming healthy.
+    max_t: int
+        the maximum number of steps
 
     Returns
     -------
-    the lettice after the last step
-
+    list: the lettice after the last step
     """
-    pass
+    temp_lettice = np.zeros(
+        shape=(len(initial_lettice), len(initial_lettice[0])),
+        dtype=np.int32
+    )
+
+    for _ in range(max_t):
+        for i in range(len(initial_lettice)):
+            for j in range(len(initial_lettice[0])):
+                ret = __find(initial_lettice)
+                if ret > 8:
+                    temp_lettice[i][j] = 0
+                elif ret > random.randint(0, 9):
+                    temp_lettice[i][j] = 1
+
+        initial_lettice = np.copy(temp_lettice)
+
+    return initial_lettice
