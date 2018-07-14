9efda6de028d6ab11b876152a7a03ca49ee6fee4
P41732 (Kammerer Lukas) <lukas.kammerer@fh-hagenberg.at>
2018-07-14 10:18:01 +0200

    #6 Add skeleton for GA.

--- Code/genetic_algorithm.py
+++ Code/genetic_algorithm.py
@@ -1,3 +1,22 @@
-# Runs the whole optimzation procedure.
+MAX_GENERATION = 100
+MUTATION_RATE = 0.15
+POPULATION_SIZE = 200
+
+
+# Runs the whole procedure
 def run():
 	pass
+
+def crossover(parent1, parent2): # Returns child
+	pass
+
+def mutate(individual): # Returns mutated individual
+	pass
+
+def select(population): # Select an individual
+	pass
+
+def initial(): # Returns the intial population size
+	pass
+
+run()
