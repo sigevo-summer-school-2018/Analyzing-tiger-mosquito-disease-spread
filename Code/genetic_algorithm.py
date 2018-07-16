c2ef7b2d51865f4e2b079965e325f8cef10940f4
P41732 (Kammerer Lukas) <lukas.kammerer@fh-hagenberg.at>
2018-07-16 06:51:25 +0200

    #6 Add selection, generational replacement and population intitialization

--- Code/genetic_algorithm.py
+++ Code/genetic_algorithm.py
@@ -1,17 +1,40 @@
 import random
 import copy
+import numpy as np
+
+import evaluation
+import automaton
 
 MAX_GENERATION = 100
 MUTATION_RATE = 0.15
 POPULATION_SIZE = 200
+TOURNAMENT_SIZE = 3
+
+INITIAL_LETTICE = np.zeros((100,100), dtype="int32")
+MAX_AUTOMATA_T = 100
+NEIGHBOURHOOD_SIZE = 8
 
 
 # Runs the whole procedure
 def run():
-    pass
+	population = initial()
+
+	for i in range(0, MAX_GENERATION):
+		population_results = map(lambda i: automaton.run(INITIAL_LETTICE, i, MAX_AUTOMATA_T), population)
+		population_qualities = map(evaluation.run, population_results)
+
+		print(f"geneneration {i}:")
+		best_individuum_idx = population_qualities.index(max(population_qualities))
+		print(f"    best individuum {population[best_individuum_idx]}")
+		print(f"    best quality {population_qualities[best_individuum_idx]}")
+		print()
+
+		population = generational_replacement(population, qualities)
+
 
 def crossover(parent1, parent2): # Returns child
     pass
+
 
 def mutate(individual): # Returns mutated individual
     if(random.random() > MUTATION_RATE):
@@ -28,11 +51,39 @@ def mutate(individual): # Returns mutated individual
     
     return mutant
 
-def select(population): # Select an individual
-    pass
 
-def initial(): # Returns the intial population size
-    pass
+def parent_id_selection(population, qualities): # Select an individual
+    tournament = map(lambda i: random.randint(0, len(population)), range(0, TOURNAMENT_SIZE))
+	return tournament.index(max(tournament))
+
+
+def generational_replacement(population, qualities):
+	elite = qualities.index(max(qualities)) # best individual always survives
+
+	new_population = [elite]
+
+	for i in range(1, len(population)):
+		p1 = parent_id_selection(population, qualities)
+		p2 = parent_id_selection(population, qualities)
+
+		child = crossover(p1, p2)
+		child = mutate(child)
+
+		new_population.add(child)
+	
+	return new_population
+
+
+def initial(): # Returns the intial population
+	return list(map(random_individuum, range(0, POPULATION_SIZE)))
+
+   
+def random_individuum():
+	return [
+		list(map(random.random, range(0, NEIGHBOURHOOD_SIZE)),
+		list(map(random.random, range(0, NEIGHBOURHOOD_SIZE)),
+	]
+
 
 def get_copy(individual):
     return copy.deepcopy(individual)
